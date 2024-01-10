140d702e35bf7586f6330ff1d30b1537e7c06d80
mirelois <79458822+mirelois@users.noreply.github.com>
2024-01-10 03:06:15 +0000

    pain

--- pg52675_pg52682/run.sh
+++ pg52675_pg52682/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-##SBATCH --time=1:00
-##SBATCH --partition=cpar
-##SBATCH --ntasks=1
-##SBATCH --constraint=k20
+#SBATCH --time=1:00
+#SBATCH --partition=cpar
+#SBATCH --ntasks=1
+#SBATCH --constraint=k20
 
 ./bin/MD < inputdata.txt
